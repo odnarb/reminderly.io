b19a80a0fa993d4ec49e328a5bc390c65555fe52
Brandon Chambers <bran.cham@gmail.com>
2019-04-19 16:26:51 -0700

    Tabs to spaces

--- logging/generate_new_log_table_statements.sql
+++ logging/generate_new_log_table_statements.sql
@@ -1,28 +1,28 @@
 -- create the tables
 SELECT
-	CONCAT('CREATE TABLE `', table_schema, '`.`', table_name, '_new` LIKE `', table_schema, '`.`', table_name, '`;')
+    CONCAT('CREATE TABLE `', table_schema, '`.`', table_name, '_new` LIKE `', table_schema, '`.`', table_name, '`;')
 FROM
-	information_schema.tables
+    information_schema.tables
 WHERE
-	table_schema = 'reminderly'
-	AND table_name LIKE BINARY 'log\_%';
+    table_schema = 'reminderly'
+    AND table_name LIKE BINARY 'log\_%';
 
 
 -- RENAME TABLE `reminderly`.`log_users_ui_groups` TO `reminderly`.`drop_log_users_ui_groups_41920190001`;
 SELECT
-	CONCAT('RENAME TABLE `', table_schema, '`.`', table_name, '` TO `', table_schema, '`.`drop_', table_name, '_', date_format( curdate(), '%m%d%Y0001'), '`;')
+    CONCAT('RENAME TABLE `', table_schema, '`.`', table_name, '` TO `', table_schema, '`.`drop_', table_name, '_', date_format( curdate(), '%m%d%Y0001'), '`;')
 FROM
-	information_schema.tables
+    information_schema.tables
 WHERE
-	table_schema = 'reminderly'
-	AND table_name LIKE BINARY 'log\_%';
+    table_schema = 'reminderly'
+    AND table_name LIKE BINARY 'log\_%';
 
 
 -- RENAME TABLE `reminderly`.`log_users_ui_groups_new` TO `reminderly`.`log_users_ui_groups`;
 SELECT
-	CONCAT('RENAME TABLE `', table_schema, '`.`', table_name, '_new` TO `', table_schema, '`.`', table_name, '`;')
+    CONCAT('RENAME TABLE `', table_schema, '`.`', table_name, '_new` TO `', table_schema, '`.`', table_name, '`;')
 FROM
-	information_schema.tables
+    information_schema.tables
 WHERE
-	table_schema = 'reminderly'
-	AND table_name LIKE BINARY 'log\_%';
+    table_schema = 'reminderly'
+    AND table_name LIKE BINARY 'log\_%';
