a5ca008ac3dd13450b788c80b2af62053e942d30
Brandon Chambers <bran.cham@gmail.com>
2019-04-19 13:47:04 -0700

    Added log table offloader script. Tested...rotates all tables with log_ prefix.

--- logging/log_table_offloader.sh
+++ logging/log_table_offloader.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+
+#create the statements from the table
+mysql -u reminderly -p'Rem!nDerly123!$' reminderly -s < generate_new_log_table_statements.sql > tmp_create_new_log_tables.sql
+
+#create our new tables
+mysql -u reminderly -p'Rem!nDerly123!$' reminderly < tmp_create_new_log_tables.sql
+
+#done with creating and renaming tables
+rm tmp_create_new_log_tables.sql
+
+#backup the log tables
+DATE=$(date +"%Y%m%d")
+mysql -u reminderly -p'Rem!nDerly123!$' -NB information_schema -e "select table_name from tables where table_name like 'drop\_%'" | xargs -I"{}" mysqldump -u reminderly -p'Rem!nDerly123!$' reminderly "{}" | gzip -c > log_backups_$DATE.gz
+
+#drop em
+mysql -u reminderly -p'Rem!nDerly123!$' -NB information_schema -e "select table_name from tables where table_name like 'drop\_%'" | xargs -I"{}" mysql -u reminderly -p'Rem!nDerly123!$' reminderly -e "DROP TABLE {}" 
