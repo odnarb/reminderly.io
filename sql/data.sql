7960119b6278376b45441e1b2dc5004095cbe0e7
Brandon Chambers <bran.cham@gmail.com>
2019-04-20 11:40:27 -0700

    Added descriptions to contact_Status table.

--- sql/data.sql
+++ sql/data.sql
@@ -41,10 +41,10 @@ INSERT INTO `data_ingest_stage` (`name`,`description`) VALUES
 
 
 INSERT INTO `contact_status` (`contact_status`) VALUES
-('not sent'),
-('queue'),
-('sent'),
-('error');
+('not sent','The message has not been sent yet.'),
+('queue','The message has been queued.'),
+('sent','The message has been sent.'),
+('error','There was an error with sending the message.');
 
 INSERT INTO `contact_method` (`contact_method`) VALUES
 ('sms'),
