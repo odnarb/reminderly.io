e86ad32793295ca8ac222c4504e1b5a597c43e77
Brandon Chambers <bran.cham@gmail.com>
2021-09-19 08:15:53 -0700

    DB - Create user and db with support for different connection processes (node.js).

--- sql/create_user_and_db.sql
+++ sql/create_user_and_db.sql
@@ -1,10 +1,18 @@
 DROP USER IF EXISTS reminderly;
 DROP DATABASE IF EXISTS reminderly;
 DROP DATABASE IF EXISTS _skeema_tmp;
+
 CREATE DATABASE reminderly;
 CREATE DATABASE _skeema_tmp;
-CREATE USER 'reminderly'@'%' IDENTIFIED BY 'Rem!nDerly123!$';
+
+CREATE USER 'reminderly'@'localhost' IDENTIFIED WITH mysql_native_password BY 'reminderly';
+CREATE USER 'reminderly'@'%' IDENTIFIED WITH mysql_native_password BY 'reminderly';
+
 GRANT ALL PRIVILEGES ON reminderly.* TO 'reminderly'@'%';
+GRANT ALL PRIVILEGES ON _skeema_tmp.* TO 'reminderly'@'%';
+
+GRANT ALL PRIVILEGES ON reminderly.* TO 'reminderly'@'localhost';
+GRANT ALL PRIVILEGES ON _skeema_tmp.* TO 'reminderly'@'localhost';
 
 FLUSH PRIVILEGES;
 USE reminderly;
